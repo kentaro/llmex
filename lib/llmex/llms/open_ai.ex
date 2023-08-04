7747a967711a7033f9a76e3536822e06a44aac6f
Kentaro Kuribayashi <kentarok@gmail.com>
2023-08-04 21:56:44 +0900

    support only chat_completions api

--- lib/llmex/llms/open_ai.ex
+++ lib/llmex/llms/open_ai.ex
@@ -10,20 +10,6 @@ defmodule Llmex.Llms.OpenAI do
   end
 
   def generate(llm, args) do
-    cond do
-      Keyword.has_key?(args, :prompt) ->
-        generate_with_prompt(llm, args)
-
-      Keyword.has_key?(args, :messages) ->
-        generate_with_messages(llm, args)
-    end
-  end
-
-  defp generate_with_prompt(llm, args) do
-    OpenAI.completions(args, llm.config)
-  end
-
-  defp generate_with_messages(llm, args) do
     messages = [messages: args[:messages].messages]
 
     OpenAI.chat_completion(
